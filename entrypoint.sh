4fce9963967c20aac13b69253307db6736a76d1c
shinhwagk <shanghai_sl@hotmail.com>
2020-03-12 20:09:32 +0800

    fix failure return 0

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,14 @@ c_password=$3
 dec_file() {
   echo "dec file ${1}";
   [ "${1: -4}" == '.enc' ] && { openssl enc -d -a -aes256 -pbkdf2 -k ${2} -in $1 -out "${1::-4}" && rm -f $1; } || {
-    echo "dec file ${1} failure."
+    echo "dec file ${1} failure."; exit 1
   }
 }
 
 enc_file() {
   echo "enc file ${1}";
   [ "${1: -4}" != '.enc' ] && { openssl enc -e -a -aes256 -pbkdf2 -k ${2} -in $1 -out "${1}.enc" && rm -f $1; } || {
-    echo "enc file ${1} failure."
+    echo "enc file ${1} failure."; exit 1
   }
 }
 
